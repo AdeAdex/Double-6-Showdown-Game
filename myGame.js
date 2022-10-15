13a68379ca5f633362ae7ef82e501f9835ca8d33
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2022-10-15 13:23:29 +0100

    Update myGame.js

--- myGame.js
+++ myGame.js
@@ -1,59 +1,62 @@
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "30%";
-  document.getElementById("main").style.marginLeft = "30%";
+  document.getElementById("openBtnContainer").style.marginLeft = "30%";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("openBtnContainer").style.marginLeft= "0";
 };
 
 
 let firstPlayer = prompt("First player enter your name", "Adex")
 let secondPlayer = prompt("Second player enter your name", "Adeolu")
+if (firstPlayer == "" || null) {firstPlayer = "Player1"};
+if (secondPlayer == "" || null) {secondPlayer = "Player2"};
+
 let counter = 0;
 let count = 0;
 function playLudo() {
     counter+= 1;
-    document.getElementById("num1").innerHTML = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("num2").innerHTML = Math.floor(Math.random() * 6) + 1;
-    if (document.getElementById("num1").innerHTML >= 6 && document.getElementById("num2").innerHTML >= 6) {
-        document.getElementById("num3").innerHTML = " You've successfully found double 6 after clicking in " + counter + " times";
-        let display = "Your output is "
-        document.getElementById("num").innerHTML = display
+    document.getElementById("output1").innerHTML = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("output2").innerHTML = Math.floor(Math.random() * 6) + 1;
+    if (document.getElementById("output1").innerHTML >= 6 && document.getElementById("output2").innerHTML >= 6) {
+        document.getElementById("displayTxt2").innerHTML = " You've successfully found double 6 after clicking in " + counter + " times";
+        let display = " Your output is "
+        document.getElementById("displayTxt1").innerHTML =  display
 		alert("Hey! save your count times or time of clicked as " + counter + " before the next player start to click ")
-        document.getElementById("num1").style.color = "green";
-        document.getElementById("num2").style.color = "green";
-        document.getElementById("num1").style.backgroundColor = "white";
-        document.getElementById("num2").style.backgroundColor = "white";
+        document.getElementById("output1").style.color = "blue";
+        document.getElementById("output2").style.color = "blue";
+        document.getElementById("output1").style.backgroundColor = "white";
+        document.getElementById("output2").style.backgroundColor = "white";
 		counter = 0;
-    } else if (document.getElementById("num1").innerHTML <= 6 && document.getElementById("num2").innerHTML <= 6) {
-        document.getElementById("num3").innerHTML = "Continue rolling the dies"
-        let display =  "Your clicked output is "
-        document.getElementById("num").innerHTML = display
-        document.getElementById("num1").style.color = "red";
-        document.getElementById("num2").style.color = "red";
-        document.getElementById("num1").style.backgroundColor = "black";
-        document.getElementById("num2").style.backgroundColor = "black";
+    } else if (document.getElementById("output1").innerHTML <= 6 && document.getElementById("output2").innerHTML <= 6) {
+        document.getElementById("displayTxt2").innerHTML = "Continue rolling the dies"
+        let display = " Your clicked output is "
+        document.getElementById("displayTxt1").innerHTML = display
+        document.getElementById("output1").style.color = "red";
+        document.getElementById("output2").style.color = "red";
+        document.getElementById("output1").style.backgroundColor = "black";
+        document.getElementById("output2").style.backgroundColor = "black";
     }
     else {
         alert("Continue rolling the dies")
     }
- document.getElementById("dGame2").textContent = "You Just Clicked " + counter + " times";
+ document.getElementById("displayTxt3").textContent = "You Just Clicked " + counter + " times";
 };
 
 
 function save() {
     let saveEl = document.getElementById("save-el"); // previous element:
-    let countStr = document.getElementById("inp").value + " - "; //anything input  + -  eg 2 -
+    let countStr = document.getElementById("input1").value + " - "; //anything input  + -  eg 2 -
     saveEl.textContent = saveEl.textContent + " " + countStr; // previous element: + space + 2 -
  };
 
 
  function save2() {
     let saveEl2 = document.getElementById("save-el2"); // previous element:
-    let countStr2 = document.getElementById("inp2").value + " - "; //anything input  + -  eg 2 -
+    let countStr2 = document.getElementById("input2").value + " - "; //anything input  + -  eg 2 -
     saveEl2.textContent = saveEl2.textContent + " " + countStr2; // previous element: + space + 2 -
  };
 
@@ -90,12 +93,14 @@ function degenerate2() {
 
 function who() {
     if (document.getElementById("totalVal").innerHTML < document.getElementById("totalVal2").innerHTML) {
-        document.getElementById("final2").innerHTML = "Congratulation! " + firstPlayer + " is the winner";
+        document.getElementById("final2").innerHTML = "Congratulation! " + firstPlayer + " is the winner" ;
+        document.getElementById("win").style.display = "block";
     } else if (document.getElementById("totalVal").innerHTML == document.getElementById("totalVal2").innerHTML) {
          document.getElementById("final2").innerHTML = "No one is the winner, you both have the same scores, play the game for atleast one more time to decide the winner"
     }
     else{
          document.getElementById("final2").innerHTML = "Congratulation! " + secondPlayer + " is the winner";
+         document.getElementById("win").style.display = "block";
     }
 };
 
@@ -103,13 +108,6 @@ function whyClick() {
     alert("The save button is used to save each of your time of clicked or click time for a successful draw and they are entered into the space provided at the side of the save button.\n\nThe generate button is used to Input each digit from your time of clicked and for every digit you enter, click the Genearte button to decide the winner and once you are done inputing them, click the Winner button to see the winner. \n\nNote: The Reset button is used to clear the generated value")
 }
 
-
-
-function playerName() {
-     document.getElementById("dft").innerHTML = firstPlayer + " " + "time of clicked for successful draw";
-     document.getElementById("dft2").innerHTML = secondPlayer + " " + "time of clicked for successful draw"
-}
-window.onload = playerName;
 
 
 
@@ -125,12 +123,15 @@ function aboutUs() {
 
 function playerName() {
     if (firstPlayer != "" && secondPlayer != "") {
-     document.getElementById("dft").innerHTML = firstPlayer + " " + "time of clicked for successful draw";
-     document.getElementById("dft2").innerHTML = secondPlayer + " " + "time of clicked for successful draw"
+     document.getElementById("playerName1").innerHTML = firstPlayer + " " + "time of clicked for successful draw";
+     document.getElementById("playerName2").innerHTML = secondPlayer + " " + "time of clicked for successful draw"
  }else{
-    alert("nothing")
+    document.getElementById("playerName1").innerHTML = "Player1 " + "time of clicked for successful draw";
+     document.getElementById("playerName2").innerHTML = "Player2 " + "time of clicked for successful draw"
  }
-};
+};window.onload = playerName;
+
+
 
 function closeMe() {
     window.opener = self;
